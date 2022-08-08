529a704b397dfda56d3d9124bf15aca61e116199
EL Gabo <harcot.sys@gmail.com>
2022-08-07 22:33:12 -0500

    Create directory Basic

---
+++ basic/operators.py
@@ -0,0 +1,7 @@
+edad = int(input("Escribe tu edad: "))
+if edad > 17: #siempre despu[es de un condicional debe haber 4 espacios
+    print('Eres mayor de edad')
+    #pass #esto lo dejo para despues, 
+else:
+    print('Eres menor de edad')
+    #pass
