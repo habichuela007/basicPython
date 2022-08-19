334d45051a847e9366fa34977f4b927d65fc46d0
EL Gabo <harcot.sys@gmail.com>
2022-08-18 22:21:34 -0500

    tresclases mas, y aprendiendo a debbugear

--- cuso-python/lambda.py
+++ cuso-python/lambda.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 def run():
     #my_list = [1,4,5,6,9,13,19,21]
     # odd = [i for i in my_list if i % 2 !=0]
@@ -13,11 +15,17 @@ def run():
     # squares = list(map(lambda x: x**2, my_list))
     # print(squares)
 
+    # my_list = [2,2,2,2,2]
+    # all_multiplied = 1
+    # for i in my_list:
+    #     all_multiplied = all_multiplied*i
+    # print(all_multiplied)
+
+    #REDUCE, lambda lleva dos parámetros, en la primera iteración va a ser el primer elemento de nuestra lista y el segundo elemento de nuestra lista, el resultado de eso se multiplica con el tercero, etc.
     my_list = [2,2,2,2,2]
-    all_multiplied = 1
-    for i in my_list:
-        all_multiplied = all_multiplied*i
+    all_multiplied = reduce(lambda a, b: a * b, my_list)
     print(all_multiplied)
+
 
 
 
